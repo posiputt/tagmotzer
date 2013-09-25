5e1dff4822ad076be7908f2afd149b2279a50372
posiputt <i@posiputt.de>
2013-09-25 19:34:12 +0200

    first commit

---
+++ tagmotzer.rb
@@ -0,0 +1,17 @@
+require 'rubygems'
+require 'id3lib'
+
+allfiles = Dir["./tagmotzer/**/*"]
+
+allfiles.each do |foo|
+  if File.extname(foo) == ".mp3"
+    tag = ID3Lib::Tag.new(foo)
+    unless tag.title == nil
+      puts tag.title
+    else
+      puts "** title not set for " + foo
+    end
+  else
+    puts "\t" + foo + " is not an mp3 file"
+  end
+end
